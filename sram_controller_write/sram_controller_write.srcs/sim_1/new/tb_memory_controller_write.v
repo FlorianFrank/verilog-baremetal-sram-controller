834b7ad5944d500d7b0f86d11318d7383bb346e1
Florian Frank <Florian.Frank@uni-passau.de>
2024-01-12 17:48:41 +0100

    Update memory controller write. allow speicificatoin of each step in the pipeline

--- sram_controller_write/sram_controller_write.srcs/sim_1/new/tb_memory_controller_write.v
+++ sram_controller_write/sram_controller_write.srcs/sim_1/new/tb_memory_controller_write.v
@@ -21,7 +21,9 @@
 
 `define INPUT_FREQUENCY 100
 `define ADDRESS_BUS_SIZE 32
-`define DATA_BUS_SIZE 16
+`define DATA_BUS_SIZE 32
+`define OUTPUT_DATA_BUS_SIZE 16
+
 `define CLOCK_CONFIG_WIDTH 16
 
 module tb_memory_controller_write(
@@ -32,20 +34,22 @@ module tb_memory_controller_write(
     reg clk = 0;
     reg start = 0;
     reg reset = 0;
-    reg [`CLOCK_CONFIG_WIDTH-1:0] teleh = `CLOCK_CONFIG_WIDTH'd50;
-    reg[`ADDRESS_BUS_SIZE-1:0] address = `ADDRESS_BUS_SIZE'h55aa55aa55;
+    reg[`ADDRESS_BUS_SIZE-1:0] address = `ADDRESS_BUS_SIZE'h55aa55aa;
     wire active;
     wire ready;
 
 
-    reg [`DATA_BUS_SIZE-1:0] value = `DATA_BUS_SIZE'hff00;
-    wire [`DATA_BUS_SIZE-1:0] dlines;
+    reg [`DATA_BUS_SIZE-1:0] value = `DATA_BUS_SIZE'haabbccdd;
+    wire [`OUTPUT_DATA_BUS_SIZE-1:0] dlines;
     wire [`ADDRESS_BUS_SIZE-1:0] alines;
     wire ce, oe, we;
 
 
     initial begin
         #2
+        reset <= 1;
+        #2
+        reset <= 0;
         start <= 1;
         #4
         start <= 0;
@@ -60,17 +64,28 @@ module tb_memory_controller_write(
     initial begin
         forever #2 clk = ~clk;
     end
+  
 
     memory_write_top_module  #(    
     .FREQ_CLK1(100),
     .FREQ_CLK2(400),
-    .ADDRESS_BUS_SIZE(32),
-    .DATA_BUS_SIZE(16),
-    .CLOCK_CONFIG_WIDTH(16))
-     memory_module_writer(.clk(clk), .start(start), .reset(reset), .value(value), .dlines(dlines), .address(address), .teleh(teleh),
-        .alines(alines), .ce(ce), .oe(oe), .we(we), .active(active), .ready(ready));
-
-    //% Input clock to trigger the always block executing the state machine.	
+    .ADDRESS_BUS_SIZE(`ADDRESS_BUS_SIZE),
+    .DATA_BUS_SIZE(`DATA_BUS_SIZE),
+    .DATA_BUS_SIZE_OUT(`OUTPUT_DATA_BUS_SIZE),
+    .tnext(10),
+    .CLOCK_CONFIG_WIDTH(16),
+    .SEPERATE_CE(0),
+    
+    .TSETUP(1),
+    .TAS(1),
+    .TWECE(1),
+    .TPWC(10),
+    .tNEXT(1) 
+    )
+    
+    
+    
+     memory_module_writer(.clk(clk), .start(start), .reset(reset), .value(value), .dlines(dlines), .address(address), .alines(alines), .ce(ce), .oe(oe), .we(we), .active(active), .ready(ready));
 
 
 endmodule
--- sram_controller_write/sram_controller_write.srcs/sources_1/new/memory_write_top_module.v
+++ sram_controller_write/sram_controller_write.srcs/sources_1/new/memory_write_top_module.v
@@ -7,12 +7,20 @@
 //% @author Florian Frank
 //% @copyright University of Passau - Chair of Computer Engineering
 module memory_write_top_module #(
-    parameter integer FREQ_CLK1=100,
-    parameter integer FREQ_CLK2=400,
-    parameter integer ADDRESS_BUS_SIZE=15,
-    parameter integer DATA_BUS_SIZE=64,
-    parameter integer DATA_BUS_SIZE_OUT=8,
-    parameter integer CLOCK_CONFIG_WIDTH=16
+    parameter integer FREQ_CLK1 = 100,
+    parameter integer FREQ_CLK2 = 400,
+    parameter integer ADDRESS_BUS_SIZE = 15,
+    parameter integer DATA_BUS_SIZE = 64,
+    parameter integer DATA_BUS_SIZE_OUT = 16,
+    parameter integer CLOCK_CONFIG_WIDTH = 16,
+    parameter integer SEPERATE_CE=0,
+
+    // Timing adjustments between different phases. (Granularity of one clock cycle)
+    parameter integer TSETUP = 1,
+    parameter integer TAS = 1,
+    parameter integer TWECE = 1,
+    parameter integer TPWC = 10,
+    parameter integer tNEXT = 1   
 ) (
     //% Input clock to trigger the always block executing the state machine.	
     input wire clk,
@@ -23,15 +31,8 @@ module memory_write_top_module #(
 
     //% Input value which should be written to the memory module.
     input wire [DATA_BUS_SIZE-1:0] value,
-    
-    input wire mode,
-    
-    
-    input wire [DATA_BUS_SIZE_OUT-1:0] puf_value_in,
-
+        
     input wire[ADDRESS_BUS_SIZE-1:0] address,
-
-    input wire[CLOCK_CONFIG_WIDTH-1:0] teleh,
 
     //% Data lines to write the values to.
     output reg[DATA_BUS_SIZE_OUT-1:0] dlines,
@@ -46,7 +47,6 @@ module memory_write_top_module #(
     //% Wire indicating that the data was written.
 
     output reg active,
-
     output reg ready
 );
 
@@ -54,29 +54,34 @@ module memory_write_top_module #(
 
 
     //% Calculates the length of the notification pulse to synchroize between FREQ_CLK1 of the management module and FREQ_CLK2 of the memory controller.
-    reg [CLOCK_CONFIG_WIDTH-1:0] STEP_SIZE_IN_NS = 1000000000/(FREQ_CLK2 *1e6) * 2;
+    reg [CLOCK_CONFIG_WIDTH-1:0] STEP_SIZE_IN_NS = 1000000000 / (FREQ_CLK2 * 1e6) * 2;
 
     reg [CLOCK_CONFIG_WIDTH-1:0] counter = 0;
     reg [3:0] state_reg;
     reg[7:0] round_ctr = 0;
     
-
-    parameter INITIALIZE    = 0;
-    parameter NEXT_ROUND    = 1;
-    parameter SET_ADDRESS 	= 2;
-    parameter ACTIVATE_CE   = 3;
-    parameter ACTIVATE_WE   = 4;
-    parameter SET_DATA 	    = 5;
-    parameter FINISH        = 6;
-    parameter MAX_NR_STATES = 7;
+    // Parameterized state values
+    parameter INITIALIZE         = 0;
+    parameter NEXT_ROUND         = 1;
+    parameter SET_ADDRESS 	     = 2;
+    parameter ACTIVATE_CE        = 3;
+    parameter ACTIVATE_WE        = 4;
+    parameter ACTIVATE_CE_WE     = 5;
+    parameter SET_DATA 	         = 6;
+    parameter FINISH             = 7;
+    
+    parameter MAX_NR_STATES      = 8;
 
 
     function is_time_expired;
         input [CLOCK_CONFIG_WIDTH-1:0] time_to_wait;
         begin
-            if(time_to_wait == 0) is_time_expired = 1;
+            if(time_to_wait == 0) begin 
+                is_time_expired = 1;
+                counter = 0;
+                end 
             else begin
-                if((counter * STEP_SIZE_IN_NS) >= (time_to_wait - STEP_SIZE_IN_NS)) begin
+                if(counter >= time_to_wait) begin
                     counter = 0;
                     is_time_expired = 1;
                 end
@@ -95,113 +100,141 @@ module memory_write_top_module #(
     
     
     reg [ADDRESS_BUS_SIZE-1:0] address_tmp;
-    reg [CLOCK_CONFIG_WIDTH-1:0] teleh_tmp;
     
     reg[7:0] clk_sync_ctr = 0;
     reg[7:0] clock_buff_ctr = 0;
-
-
+    
     //% Initial block initializes all values to the default values.
-    initial begin
-
-        value_tmp   <= 0;
-        address_tmp <= 0;
-        teleh_tmp   <= 0;
-
-        ready       <= 0;
-        active      <= 0;
-        alines      <= 15'h0;
-        dlines      <= 8'h0;
-        we 		    <= 1;
-        oe 		    <= 1;
-        ce 		    <= 1;
-
-        counter    <= 0;
-        state_reg  <= 0;
+    initial begin 
+        counter <= 0;
+        state_reg <= 0;
         clk_sync_ctr <= 0;
+        value_tmp <= 0;
+        address_tmp <= 0;
+        ready <= 0;
+        active <= 0;
+        alines <= 15'h0;
+        dlines <= 8'h0;
+        we <= 1;
+        oe <= 1;
+        ce <= 1;        
+        end
+
+    always @ (posedge clk or posedge reset) begin
+        if (reset) begin
+            counter <= 0;
+            state_reg <= 0;
+            clk_sync_ctr <= 0;
+            value_tmp <= 0;
+            address_tmp <= 0;
+            ready <= 0;
+            active <= 0;
+            alines <= 15'h0;
+            dlines <= 8'h0;
+            we <= 1;
+            oe <= 1;
+            ce <= 1;
+        end
     end
     
-    
-
-
 
     //% @brief Always block executes a write operation per time using a constant value and iterates the address after each loop.
     always @ (posedge clk) begin
 
         case (state_reg)
+        
             INITIALIZE: begin
+            
                 ready <= 0;
+            
                 if(start == 1) begin
+                
                     value_tmp <= value;
-                    out_value <= puf_value_in;
                     address_tmp <= address;
-                    teleh_tmp <= teleh;
 
                     ce <= 1;
                     oe <= 1;
                     we <= 1;
-                    if(mode == 0) // Simple forward mode
-                        state_reg <= NEXT_ROUND;
-                    else    // PUF MODE 
-                        state_reg <= SET_ADDRESS;
-                    active <= 1;
+                    
+                    
                     clock_buff_ctr <= 0;
-                end else active <= 0;
+                    out_value <= value[DATA_BUS_SIZE_OUT-1:0];
+                    active <= 1;
+                   
+                   
+                    if(is_time_expired(TSETUP-1))  state_reg <= SET_ADDRESS;
+               end
+               else
+                    active <= 0;
+
             end
             
             NEXT_ROUND: begin
+                
                 ce <= 1;
                 we <= 1;
                 oe <= 1;
+                
                 out_value <= value_tmp[clock_buff_ctr*DATA_BUS_SIZE_OUT+:DATA_BUS_SIZE_OUT];
-                address_tmp <= address_tmp + 1;
+                if(is_time_expired(tNEXT-1)) state_reg <= SET_ADDRESS;
             end
 
             SET_ADDRESS: begin
                 clock_buff_ctr <= clock_buff_ctr + 1; 
                 alines <= address_tmp;
-                if(is_time_expired(0))
-                    state_reg <= ACTIVATE_CE;
+                
+                if(is_time_expired(TAS-1)) begin
+                    if (SEPERATE_CE == 0)
+                        state_reg <= ACTIVATE_CE;
+                    else 
+                        state_reg <= ACTIVATE_CE_WE;
+                end
             end
 
             ACTIVATE_CE: begin
                 ce <= 0;
-                if(is_time_expired(0))
+                if(is_time_expired(TWECE-1))
                     state_reg <= ACTIVATE_WE;
             end
 
             ACTIVATE_WE: begin
                 we <= 0;
-                if(is_time_expired(teleh_tmp))
+                if(is_time_expired(TPWC-1))
+                    state_reg <= SET_DATA;
+            end
+            
+            ACTIVATE_CE_WE: begin
+                we <= 0;
+                ce <= 0;
+                if(is_time_expired(TPWC-1))
                     state_reg <= SET_DATA;
             end
 
             SET_DATA: begin
                 dlines <= out_value;
-                if(is_time_expired(0)) begin
-                 if(mode == 0 && clock_buff_ctr < DATA_BUS_SIZE/DATA_BUS_SIZE_OUT)
+                address_tmp <= address_tmp + 1;
+                 if(clock_buff_ctr < DATA_BUS_SIZE/DATA_BUS_SIZE_OUT)
                     state_reg <= NEXT_ROUND;
                  else 
                     state_reg <= FINISH;
-                end
             end
 
             FINISH: begin
                 ce <= 1;
                 we <= 1;
                 oe <= 1;
+                active <= 0;
+                ready <= 1;
 
 
                 if(clk_sync_ctr < FREQ_CLK2/FREQ_CLK1) begin
                     clk_sync_ctr <= clk_sync_ctr + 1;
-                    ready <= 1;
                 end else begin
                      clk_sync_ctr <= 0;
-                     ready <= 0;
-                     active <= 0;
-                    state_reg <= INITIALIZE;
+                     state_reg <= INITIALIZE;
                 end
-            end
+          end
+                      
         endcase
     end
 
